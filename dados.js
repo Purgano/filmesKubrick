b5e68b0e71acf9c97ed252f4f9b1dfcb88c23276
Purgano <151034436+Purgano@users.noreply.github.com>
2024-09-10 11:37:16 -0300

    Update dados.js
    adicionado todos os demais filmes

--- dados.js
+++ dados.js
@@ -1,14 +1,76 @@
 const filmesKubrick = [
     {
+      titulo: "Medo e Desejo",
+      ano: 1953,
+      genero: "Drama de Guerra",
+      sinopse: "O primeiro longa-metragem de Kubrick, um drama de guerra experimental e de baixo orçamento, explorando temas como medo, violência e a condição humana.",
+      wiki: "https://pt.wikipedia.org/wiki/Fear_and_Desire",
+      imdb: "https://www.imdb.com/title/tt0045991/",
+      tags: "guerra experimental baixo orçamento Fear and Desire"
+    },
+    {
+      titulo: "A Morte Passou por Perto",
+      ano: 1955,
+      genero: "Noir, Drama",
+      sinopse: "Um filme noir sobre um boxeador que se envolve com uma mulher perigosa e um crime.",
+      wiki: "https://en.wikipedia.org/wiki/Killer's_Kiss",
+      imdb: "https://www.imdb.com/title/tt0048237/",
+      tags: "noir crime boxeador Killer's Kiss"
+    },	
+	{
+      titulo: "O Grande Golpe",
+      ano: 1956,
+      genero: "Crime, Drama",
+      sinopse: "Um clássico filme noir sobre um grupo de criminosos que planejam um roubo audacioso em um hipódromo.",
+      wiki: "https://pt.wikipedia.org/wiki/The_Killing",
+      imdb: "https://www.imdb.com/title/tt0048236/",
+      tags: "crime noir roubo hipódromo The Killing"
+   },
+   {
+     titulo: "Glória Feita de Sangue",
+     ano: 1957,
+     genero: "Drama, Biografia",
+     sinopse: "Um drama biográfico sobre a vida do boxeador Emile Griffith, que se tornou campeão mundial após uma tragédia.",
+     wiki: "https://pt.wikipedia.org/wiki/Paths_of_Glory",
+     imdb: "https://www.imdb.com/title/tt0049575/",
+     tags: "boxe biografia drama Paths of Glory"
+   },	
+   {
+     titulo: "Spartacus",
+     ano: 1960,
+     genero: "Épico Histórico",
+     sinopse: ​​Um épico histórico sobre a rebelião de escravos liderada por Spartacus contra a República Romana.",
+     wiki: "https://pt.wikipedia.org/wiki/Spartacus_(filme)",
+     imdb: "https://www.imdb.com/title/tt0054385/",
+     tags: "escravos rebelião roma espartacus"
+	},	
+	{
+      titulo: "Lolita",
+      ano: 1962,
+      genero: "Drama, Romance",
+      sinopse: "Uma adaptação do controverso romance de Vladimir Nabokov sobre um professor que se apaixona por sua enteada adolescente.",
+      wiki: "https://pt.wikipedia.org/wiki/Lolita_(filme_de_1962)",
+      imdb: "https://www.imdb.com/title/tt0056282/",
+      tags: "romance controverso adolescente ninfeta"
+	},	
+	{
+	  titulo: "Dr. Fantástico",
+      ano: 1964,
+      genero: "Comédia Negra, Ficção Científica",
+      sinopse: "Uma comédia negra satírica sobre a Guerra Fria, onde um general da Força Aérea dos Estados Unidos ordena um ataque nuclear à União Soviética, desencadeando uma cadeia de eventos que ameaça a destruição do mundo.",
+      wiki: "https://pt.wikipedia.org/wiki/Dr._Strangelove",
+      imdb: "https://www.imdb.com/title/tt0057012/",
+      tags: "guerra fria comédia nuclear satira Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb"
+	},	
+	{
       titulo: "2001: Uma Odisseia no Espaço",
       ano: 1968,
       genero: "Ficção Científica",
       sinopse: "Uma estrutura imponente preta fornece uma conexão entre o passado e o futuro nesta adaptação enigmática de um conto reverenciado de ficção científica do autor Arthur C. Clarke. Quando o Dr. Dave Bowman e outros astronautas são enviados para uma misteriosa missão, os chips de seus computadores começam a mostrar um comportamento estranho, levando a um tenso confronto entre homem e máquina que resulta em uma viagem alucinante no espaço e no tempo.",
       wiki:"https://pt.wikipedia.org/wiki/2001:_A_Space_Odyssey",
       imdb:"https://www.imdb.com/title/tt0062622/",
-      tags: "espaço espaco passado futuro viagem viajem  " 
+      tags: "espaço espaco passado futuro viagem viajem A Space Odyssey" 
     },
-
     {
       titulo: "Laranja Mecânica",
       ano: 1971,
@@ -16,9 +78,17 @@ const filmesKubrick = [
       sinopse: "Um futuro distópico onde a violência é endêmica, o jovem Alex passa as noites com uma gangue de amigos delinquente. Depois que é preso, se submete a uma técnica de modificação de comportamento para poder ganhar sua liberdade.",
       wiki:"https://pt.wikipedia.org/wiki/A_Clockwork_Orange",
       imdb: "https://www.imdb.com/title/tt0066921/",
-      tags: " futuro violencia alex noites gangues brigas" 
+      tags: "futuro violencia alex noites gangues brigas A Clockwork Orange" 
+    },	
+	{
+      titulo: "Barry Lyndon",
+      ano: 1975,
+      genero: "Drama Histórico",
+      sinopse: "Uma épica história de ascensão e queda de um aventureiro do século XVIII, que se envolve em uma série de eventos dramáticos e romances.",
+      wiki: "https://pt.wikipedia.org/wiki/Barry_Lyndon",
+      imdb: "https://www.imdb.com/title/tt0071415/",
+      tags: "histórico aventura século 18 drama"
     },
-
     {
       titulo: "O Iluminado",
       ano: 1980,
@@ -26,17 +96,24 @@ const filmesKubrick = [
       sinopse: "Jack Torrance se torna caseiro de inverno do isolado Hotel Overlook, nas montanhas do Colorado, na esperança de curar seu bloqueio de escritor. Ele se instala com a esposa Wendy e o filho Danny, que é atormentando por premonições. Jack não consegue escrever e as visões de Danny se tornam mais perturbadoras. O escritor descobre os segredos sombrios do hotel e começa a se transformar em um maníaco homicida, aterrorizando sua família.",
       wiki:"https://pt.wikipedia.org/wiki/The_Shining" ,
       imdb:"https://www.imdb.com/title/tt0081505/",
-      tags: " hotel inverso machado jack perturbado psicologico " 
+      tags: "hotel inverso machado jack perturbado psicologico The Shining" 
     },
-
     {
-        titulo: "Nascido Para Matar",
-        ano: 1987,
-        genero: "Drama, Guerra",
-        sinopse: "O soldado Davis, batizado de 'Joker' pelo sargento linha-dura Hartman, suporta os rigores do treinamento básico e tenta ajudar o ridicularizado colega Lawrence. Na preparação, Hartman emprega métodos abusivos para transformar seus recrutas em verdadeiras máquinas de guerra. Lawrence não aguenta a pressão, mas Joker se forma no Corpo de Fuzileiros Navais e é enviado para o Vietnã como jornalista, cobrindo e, eventualmente, participando da sangrenta Batalha de Hué.",
-        wiki:"https://pt.wikipedia.org/wiki/Full_Metal_Jacket",
-        imdb:"https://www.imdb.com/title/tt0093058/",
-        tags: "tiro arma soldado treinamento"
-    }
+      titulo: "Nascido Para Matar",
+      ano: 1987,
+      genero: "Drama, Guerra",
+      sinopse: "O soldado Davis, batizado de 'Joker' pelo sargento linha-dura Hartman, suporta os rigores do treinamento básico e tenta ajudar o ridicularizado colega Lawrence. Na preparação, Hartman emprega métodos abusivos para transformar seus recrutas em verdadeiras máquinas de guerra. Lawrence não aguenta a pressão, mas Joker se forma no Corpo de Fuzileiros Navais e é enviado para o Vietnã como jornalista, cobrindo e, eventualmente, participando da sangrenta Batalha de Hué.",
+      wiki:"https://pt.wikipedia.org/wiki/Full_Metal_Jacket",
+      imdb:"https://www.imdb.com/title/tt0093058/",
+      tags: "tiro arma soldado treinamento Full Metal Jacket"
+    },	
+	{
+     titulo: "De Olhos Bem Fechados",
+     ano: 1999,
+     genero: "Drama Psicológico, Mistério",
+     sinopse: "Um thriller psicológico que explora temas de sexualidade, desejo e traição em um casal aparentemente perfeito.",
+     wiki: "https://pt.wikipedia.org/wiki/Eyes_Wide_Shut",
+     imdb: "https://www.imdb.com/title/tt0119094/",
+     tags: "psicológico mistério sexualidade Eyes Wide Shut"
+    }  
   ];
-
